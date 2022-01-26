3d7c96c9b322397e03368500bbf240a179d1e9de
satas99 <80315053+satas99@users.noreply.github.com>
2022-01-26 23:27:34 +0300

    Delete gameApp directory

--- gameApp/src/gameApp/business/abstracts/GamerService.java
+++
@@ -1,9 +0,0 @@
-package gameApp.business.abstracts;
-
-import gameApp.entities.concretes.Gamer;
-
-public interface GamerService {
-	void add(Gamer gamer);
-	void update(Gamer gamer);
-	void delete(int id);
-}
